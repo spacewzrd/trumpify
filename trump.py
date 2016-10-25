45f0877ea8ad205f931ca643340cb5701e43a00e
Daniel Rendon <daniel.rendonfc@gmail.com>
2016-10-25 16:59:28 -0500

    added 'knows we all need to Cyber better'

--- trump.py
+++ trump.py
@@ -5,7 +5,7 @@ def trumpify(x):
     return '“Nobody %s more than me.” – Donald Trump' % x
 
 if __name__ == "__main__":
-    things = ['repects women', 'understands the horror of nuclear', 'reads the Bible', ]
+    things = ['repects women', 'understands the horror of nuclear', 'reads the Bible', 'knows we all need to Cyber better',]
     print(trumpify(random.choice(things)))
     # for thing in things:
     #     print(trumpify(thing))
